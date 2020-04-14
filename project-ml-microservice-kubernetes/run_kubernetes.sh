aa59e133892ab35259af754614e75d524b91492b
André Brinkop <andre@brinkop.de>
2020-04-15 00:54:59 +0200

    implement run kubernetes_script to download app image from docker hub and to deploy it to Kubernetes

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,22 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerimage=sklearnflaskapp
+dockerpath=abrinkop/$dockerimage
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment $dockerimage --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+pod_name=$(kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}')
+echo Name of the Pod: $pod_name
+kubectl get pods
+
+# Wait until pod is ready
+kubectl wait --for=condition=Ready pod/$pod_name
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $pod_name 8000:80
