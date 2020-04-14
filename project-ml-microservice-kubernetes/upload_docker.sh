90bb2b7582d902ef955a61b82deea293606d91c0
André Brinkop <andre@brinkop.de>
2020-04-15 00:11:19 +0200

    implement upload_docker script to automatically upload the built image to docker hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,15 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerimage=sklearnflaskapp
+dockerpath=abrinkop/$dockerimage
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $dockerimage:latest $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
