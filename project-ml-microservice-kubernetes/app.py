76a4c8b59e2137c41fbe33c454b0d6f4764298ba
André Brinkop <andre@brinkop.de>
2020-04-14 23:41:22 +0200

    add output log after running make_prediciton to output txt files

--- project-ml-microservice-kubernetes/app.py
+++ project-ml-microservice-kubernetes/app.py
@@ -63,7 +63,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # Log the output prediction value
-    LOG.info(f"Prediction value: \n{prediction[0]}")
+    LOG.info(f"Predicted value: \n{prediction[0]}")
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
